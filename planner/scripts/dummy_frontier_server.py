323319e79018580c76d7fe616730106069a0f479
AtharvaJ11 <atharva.jamsandekar@gmail.com>
2024-04-13 15:26:25 -0400

    Changes

--- planner/scripts/dummy_frontier_server.py
+++ planner/scripts/dummy_frontier_server.py
@@ -9,6 +9,7 @@ def frontier_cb(request):
         pose.header.frame_id = 'map'
         pose.header.stamp = rospy.Time.now()
         pose.pose.position.x =5
+        pose.pose.position.y =5
         return frontier_goalResponse(pose)
     else:
         return None
